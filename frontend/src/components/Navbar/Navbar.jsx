c9f27cb938255d6842308ff69d5f34050e1f06b2
Umamah Hussain <152015569+umamahhussain@users.noreply.github.com>
2024-08-05 12:17:21 +0500

    Add files via upload

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,42 +1,88 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { UserContext } from '../../App';
+import M from "materialize-css"
  
 const Navbar = () => {
   const navigate = useNavigate();
-  const { state, dispatch } = useContext( UserContext )
-
-  const RenderNavbar = () =>{
-    if( state ){
-      return[
-        <li> <Link Link to = "/home" > Home </Link> </li>,
-        <li> <Link Link to = "/invoice" > Invoice </Link> </li>,
-        <li> <Link Link to = "/role" > Roles </Link> </li>,
-        <li>
-        <a className="btn-floating btn-large pulse #616161 grey darken-2" type="submit" name="action" onClick={() => {
-          localStorage.clear();
-          dispatch( {type: "CLEAR"} )
-          navigate('/login');
-        }}>
-        <i class="material-icons">exit_to_app</i>
-        </a>
-      </li>
-      ]}
-      else {
-        return[
-          <li> <Link to = "/signup" > Signup </Link> </li>,
-          <li> <Link to = "/login" > Login </Link> </li>
-        ]
+  const { state, dispatch } = useContext(UserContext);
+ 
+  useEffect(() => {
+    M.AutoInit(); //
+  })
+ 
+ 
+  const navigations = [
+    {
+      path: '/home',
+      label: 'Home',
+      type: 'link'
+    },
+    {
+      path: '/invoice',
+      label: 'Invoice',
+      type: 'link'
+    },
+    {
+      path: '/roles',
+      label: 'Roles',
+      type: 'link'
+    },
+    {
+      path: '/logout',
+      label: 'Log Out',
+      type: 'button',
+      fn: () => {
+        localStorage.clear();
+        dispatch({ type: 'CLEAR' });
+        navigate('/login');
       }
-  }
+    }
+  ];
+ 
+  const RenderNavbar = () => {
+    if (state) {
+      return navigations.map( ( navItem, index ) => {
+        if ( navItem.type === 'link' ) {
+          return (
+            <li key = {index}>
+              <Link to= { navItem.path } > { navItem.label } </Link>
+            </li>
+          );
+        } else if ( navItem.type === 'button' ) {
+          return (
+            <li key = { index } >
+              <a
+                className="btn-floating btn-small btn tooltipped pulse #616161 grey darken-2"
+                type="submit"
+                name="action"
+                data-position="bottom" data-tooltip="Log Out"
+                onClick={navItem.fn}
+              >
+                <i className="material-icons">exit_to_app</i>
+              </a>
+            </li>
+          );
+        }
+        return null; // Return null if the type does not match
+      });
+    } else {
+      return (
+        <>
+          <li><Link to="/signup">Signup</Link></li>
+          <li><Link to="/login">Login</Link></li>
+        </>
+      );
+    }
+  };
  
   return (
-    <div >
-      <nav style={{ backgroundColor: 'gray'}}>
+    <div>
+      <nav style={{ backgroundColor: 'gray' }}>
         <div className="nav-wrapper" style={{ backgroundColor: 'gray', marginLeft: '6px' }}>
           <a className="brand-logo audiowide-regular"><Link to="/home">Systems</Link></a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-           {RenderNavbar()}
+            {RenderNavbar()}
           </ul>
         </div>
       </nav>
@@ -45,4 +91,3 @@ const Navbar = () => {
 };
  
 export default Navbar;
- 
